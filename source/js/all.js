950c14e82a2e10bd029657714faebf3b743d64c2
GmWuWork <gmwu185@gmail.com>
2019-09-08 07:57:02 +0800

    Gulp與Git-git init

---
+++ source/js/all.js
@@ -0,0 +1,9 @@
+let newFun = () => {
+  console.log('newFun run Webservice');
+};
+
+newFun();
+
+$(function(){
+  alert('jquery is runing')
+})
---
+++ source/js/alljs2.js
@@ -0,0 +1,5 @@
+let newFun2 = () => {
+  console.log('newFun2 run Webservice');
+};
+
+newFun2();
